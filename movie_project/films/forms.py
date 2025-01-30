e70ff30fc2197050e88776bdbdab36f54bb3f828
Jeff555max <vadik98765@gmail.com>
2025-01-30 20:13:06 +0500

    3

--- movie_project/films/forms.py
+++ movie_project/films/forms.py
@@ -4,9 +4,10 @@ from .models import Film
 class FilmForm(forms.ModelForm):
     class Meta:
         model = Film
-        fields = ['title', 'description', 'review']
+        fields = ['title', 'description', 'review', 'poster']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название фильма'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите описание фильма', 'rows': 3}),
             'review': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Введите ваш отзыв', 'rows': 5}),
+            'poster': forms.ClearableFileInput(attrs={'class': 'form-control'})
         }
