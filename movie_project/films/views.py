454aa33189ac13e367fb0a4669a9adbb6db75a22
Jeff555max <vadik98765@gmail.com>
2025-01-30 20:15:09 +0500

    3

---
+++ movie_project/films/views.py
@@ -0,0 +1,18 @@
+# views.py
+from django.shortcuts import render, redirect
+from .forms import FilmForm
+from .models import Film
+
+def add_film(request):
+    if request.method == 'POST':
+        form = FilmForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('show_films')
+    else:
+        form = FilmForm()
+    return render(request, 'films/add_film.html', {'form': form})
+
+def show_films(request):
+    films = Film.objects.all()
+    return render(request, 'films/show_films.html', {'films': films})
